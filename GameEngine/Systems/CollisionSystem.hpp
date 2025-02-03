7f3a3523b7a4f7a9a85c069a5b32b36feeb669dc
Hugues Lejeune <hugues.lejeune@epitech.eu>
2025-02-03 18:34:05 +0100

    feat(ecs): refactor collision system

--- GameEngine/Systems/CollisionSystem.hpp
+++ GameEngine/Systems/CollisionSystem.hpp
@@ -19,15 +19,6 @@ class CollisionSystem : public ASystem
         CollisionSystem() = default;
         ~CollisionSystem() = default;
 
-        struct HitBox {
-            float minX;
-            float minY;
-            float maxX;
-            float maxY;
-
-            HitBox(float minX, float minY, float maxX, float maxY) : minX(minX), minY(minY), maxX(maxX), maxY(maxY) {}
-        };
-
         void update(RegistryManager &manager, std::shared_ptr<AGraphical> &graphical, float elapsedSeconds) override
         {
             (void) graphical;
@@ -37,99 +28,143 @@ class CollisionSystem : public ASystem
             auto &colliders = manager.getComponents<comp::Collider>();
 
             for (size_t i = 0; i < positions.size(); i++) {
-                if (positions[i] && colliders[i]) {
-                    colliders[i]->collidePosX = false;
-                    colliders[i]->collideNegX = false;
-                    colliders[i]->collidePosY = false;
-                    colliders[i]->collideNegY = false;
+                if (!positions[i] || !colliders[i]) {
+                    continue;
+                }
 
-                    for (size_t j = 0; j < positions.size(); j++) {
-                        if (i == j) {
-                            continue;
-                        }
-                        if (positions[j] && colliders[j]) {
-                            auto layerI = std::find(colliders[j]->collidingLayers.begin(), colliders[j]->collidingLayers.end(), colliders[i]->layer);
-                            auto layerJ = std::find(colliders[i]->collidingLayers.begin(), colliders[i]->collidingLayers.end(), colliders[j]->layer);
+                colliders[i]->collidePosX = false;
+                colliders[i]->collideNegX = false;
+                colliders[i]->collidePosY = false;
+                colliders[i]->collideNegY = false;
 
-                            if (layerI == colliders[j]->collidingLayers.end() && layerJ == colliders[i]->collidingLayers.end()) {
-                                colliders[i]->collidePosX = false;
-                                colliders[i]->collideNegX = false;
-                                colliders[i]->collidePosY = false;
-                                colliders[i]->collideNegY = false;
-                            } else {
-                                HitBox hbI(
-                                    positions[i]->x - colliders[i]->width / 2,
-                                    positions[i]->y - colliders[i]->height / 2,
-                                    positions[i]->x + colliders[i]->width / 2,
-                                    positions[i]->y + colliders[i]->height / 2
-                                );
-                                HitBox hbJ(
-                                    positions[j]->x - colliders[j]->width / 2,
-                                    positions[j]->y - colliders[j]->height / 2,
-                                    positions[j]->x + colliders[j]->width / 2,
-                                    positions[j]->y + colliders[j]->height / 2
-                                );
-
-                                std::string collisionFace = "None";
-                                float minPenetration = std::numeric_limits<float>::max();
-
-                                if (hbI.minX <= hbJ.maxX && hbI.maxX >= hbJ.minX && hbI.minY <= hbJ.maxY && hbI.maxY >= hbJ.minY) {
-                                    float penetrationPosX = hbI.maxX - hbJ.minX;
-                                    float penetrationNegX = hbJ.maxX - hbI.minX;
-                                    float penetrationPosY = hbI.maxY - hbJ.minY;
-                                    float penetrationNegY = hbJ.maxY - hbI.minY;
-
-                                    if (penetrationPosX >= 0 && penetrationPosX < minPenetration) {
-                                        minPenetration = penetrationPosX;
-                                        collisionFace = "PosX";
-                                    }
-                                    if (penetrationNegX >= 0 && penetrationNegX < minPenetration) {
-                                        minPenetration = penetrationNegX;
-                                        collisionFace = "NegX";
-                                    }
-                                    if (penetrationPosY >= 0 && penetrationPosY < minPenetration) {
-                                        minPenetration = penetrationPosY;
-                                        collisionFace = "PosY";
-                                    }
-                                    if (penetrationNegY >= 0 && penetrationNegY < minPenetration) {
-                                        minPenetration = penetrationNegY;
-                                        collisionFace = "NegY";
-                                    }
-                                }
-
-                                if (minPenetration == 0) {
-                                    collisionFace = "None";
-                                }
-                                if (collisionFace == "PosX") {
-                                    colliders[i]->collidePosX = true;
-                                } else if (collisionFace == "NegX") {
-                                    colliders[i]->collideNegX = true;
-                                } else if (collisionFace == "PosY") {
-                                    colliders[i]->collidePosY = true;
-                                } else if (collisionFace == "NegY") {
-                                    colliders[i]->collideNegY = true;
-                                }
-
-                                if (velocities.size() > i && velocities[i]) {
-                                    if (colliders[i]->collidePosX) {
-                                        positions[i]->x = hbJ.minX - colliders[i]->width / 2;
-                                    }
-                                    if (colliders[i]->collideNegX) {
-                                        positions[i]->x = hbJ.maxX + colliders[i]->width / 2;
-                                    }
-                                    if (colliders[i]->collidePosY) {
-                                        positions[i]->y = hbJ.minY - colliders[i]->height / 2;
-                                    }
-                                    if (colliders[i]->collideNegY) {
-                                        positions[i]->y = hbJ.maxY - colliders[i]->height / 2;
-                                    }
-                                }
-                            }
-                        }
+                for (size_t j = 0; j < positions.size(); j++) {
+                    if (i == j) {
+                        continue;
                     }
 
+                    if (!positions[j] || !colliders[j]) {
+                        continue;
+                    }
+
+                    auto layerI = std::find(colliders[j]->collidingLayers.begin(), colliders[j]->collidingLayers.end(), colliders[i]->layer);
+                    auto layerJ = std::find(colliders[i]->collidingLayers.begin(), colliders[i]->collidingLayers.end(), colliders[j]->layer);
+
+                    if (layerI == colliders[j]->collidingLayers.end() && layerJ == colliders[i]->collidingLayers.end()) {
+                        continue;
+                    }
+
+                    HitBox hbI(
+                        positions[i]->x - colliders[i]->width / 2,
+                        positions[i]->y - colliders[i]->height / 2,
+                        positions[i]->x + colliders[i]->width / 2,
+                        positions[i]->y + colliders[i]->height / 2
+                    );
+                    HitBox hbJ(
+                        positions[j]->x - colliders[j]->width / 2,
+                        positions[j]->y - colliders[j]->height / 2,
+                        positions[j]->x + colliders[j]->width / 2,
+                        positions[j]->y + colliders[j]->height / 2
+                    );
+
+                    CollisionFace collisionFace = findCollidingFace(hbI, hbJ);
+
+                    switch (collisionFace) {
+                        case CollisionFace::POSX:
+                            colliders[i]->collidePosX = true;
+                            break;
+                        case CollisionFace::NEGX:
+                            colliders[i]->collideNegX = true;
+                            break;
+                        case CollisionFace::POSY:
+                            colliders[i]->collidePosY = true;
+                            break;
+                        case CollisionFace::NEGY:
+                            colliders[i]->collideNegY = true;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (velocities.size() > i && velocities[i]) {
+                        switch (collisionFace) {
+                            case CollisionFace::POSX:
+                                positions[i]->x = hbJ.minX - colliders[i]->width / 2;
+                                break;
+                            case CollisionFace::NEGX:
+                                positions[i]->x = hbJ.maxX + colliders[i]->width / 2;
+                                break;
+                            case CollisionFace::POSY:
+                                positions[i]->y = hbJ.minY - colliders[i]->height / 2;
+                                break;
+                            case CollisionFace::NEGY:
+                                positions[i]->y = hbJ.maxY + colliders[i]->height / 2;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
                 }
             }
+        }
+
+    private:
+
+        struct HitBox {
+            float minX;
+            float minY;
+            float maxX;
+            float maxY;
+
+            HitBox(float minX, float minY, float maxX, float maxY) : minX(minX), minY(minY), maxX(maxX), maxY(maxY) {}
+        };
+
+        enum CollisionFace
+        {
+            NONE,
+            POSX,
+            NEGX,
+            POSY,
+            NEGY
+        };
+
+        CollisionFace findCollidingFace(HitBox const &h1, HitBox const &h2)
+        {
+            CollisionFace collisionFace = CollisionFace::NONE;
+            bool overlapX = h1.minX <= h2.maxX && h1.maxX >= h2.minX;
+            bool overlapY = h1.minY <= h2.maxY && h1.maxY >= h2.minY;
+
+            if (!overlapX || !overlapY) {
+                return CollisionFace::NONE;
+            }
+
+            float minPenetration = std::numeric_limits<float>::max();
+            float penetrationPosX = h1.maxX - h2.minX;
+            float penetrationNegX = h2.maxX - h1.minX;
+            float penetrationPosY = h1.maxY - h2.minY;
+            float penetrationNegY = h2.maxY - h1.minY;
+
+            if (penetrationPosX >= 0 && penetrationPosX < minPenetration) {
+                minPenetration = penetrationPosX;
+                collisionFace = CollisionFace::POSX;
+            }
+            if (penetrationNegX >= 0 && penetrationNegX < minPenetration) {
+                minPenetration = penetrationNegX;
+                collisionFace = CollisionFace::NEGX;
+            }
+            if (penetrationPosY >= 0 && penetrationPosY < minPenetration) {
+                minPenetration = penetrationPosY;
+                collisionFace = CollisionFace::POSY;
+            }
+            if (penetrationNegY >= 0 && penetrationNegY < minPenetration) {
+                minPenetration = penetrationNegY;
+                collisionFace = CollisionFace::NEGY;
+            }
+
+            if (minPenetration == 0) {
+                return CollisionFace::NONE;
+            }
+
+            return collisionFace;
         }
 
 };
