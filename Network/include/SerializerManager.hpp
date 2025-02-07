aebab0115aaeffad24adfeae0bbe0cdd33f70526
HuguesLej <hugues.lejeune@epitech.eu>
2025-02-07 01:45:59 +0100

    feat(network): add SerializerManager to handle packets serialization and deserialization

---
+++ Network/include/SerializerManager.hpp
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2025
+** RRR-Type [WSL: Ubuntu]
+** File description:
+** SerializerManager
+*/
+
+#ifndef SERIALIZER_MANAGER_HPP_
+    #define SERIALIZER_MANAGER_HPP_
+
+    #include <any>
+    #include <cstdint>
+    #include <exception>
+    #include <functional>
+    #include <sstream>
+    #include <typeindex>
+    #include <unordered_map>
+    #include <vector>
+
+class SerializerManager
+{
+    public:
+
+        class Error : public std::exception
+        {
+            public:
+                Error() = default;
+                ~Error() = default;
+
+                const char *what() const noexcept override;
+        };
+
+        SerializerManager() = delete;
+        ~SerializerManager() = delete;
+
+        static void registerSerializer(const std::type_index type, std::function<std::vector<uint8_t>(const std::any &)> serialize,
+            std::function<std::any(const std::vector<uint8_t> &)> deserialize);
+
+        static std::vector<uint8_t> serialize(const std::any &data);
+        static std::any deserialize(const std::vector<uint8_t> &data);
+
+    private:
+        static std::unordered_map<std::type_index,
+            std::pair<std::function<std::vector<uint8_t>(const std::any &)>, std::function<std::any(const std::vector<uint8_t> &)>>> _serializers;
+};
+
+#endif /* !SERIALIZER_MANAGER_HPP_ */
